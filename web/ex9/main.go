3e6917ef373f7949e9a77c831c6e65bc5ee14af8
Evgeniy <n0fr1@mail.ru>
2022-02-04 21:30:12 +0300

    fixed example 9

--- web/ex9/main.go
+++ web/ex9/main.go
@@ -7,8 +7,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"time"
 )
@@ -22,26 +20,10 @@ type Employee struct {
 type Handler struct {
 }
 
-//add
-type UploadHandler struct {
-	UploadDir string
-}
-
-//add
-
 func main() {
 
 	handler := &Handler{}
 	http.Handle("/", handler)
-
-	//add
-	uploadHandler := &UploadHandler{
-		UploadDir: "upload",
-	}
-
-	http.Handle("/upload", uploadHandler)
-
-	//add
 
 	srv := &http.Server{
 		Addr:         ":8000",
@@ -88,32 +70,3 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		employee.Salary,
 	)
 }
-
-//add
-func (h *UploadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	file, header, err := r.FormFile("file") //file - io.Reader - для потокового считывания файла. header - различная доп.информация о файле. err - если r.FormFile - не смог считать файл из переданной формы
-	if err != nil {
-		http.Error(w, "Unable to read file - 1", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
-
-	data, err := ioutil.ReadAll(file) //считываем содержимое файла в переменную data []byte для последующей записи на диск
-	if err != nil {
-		http.Error(w, "Unable to read file - 2", http.StatusBadRequest)
-		return
-	}
-
-	filePath := h.UploadDir + "/" + header.Filename //задаем путь, который будет иметь сохраненный на диск файл.
-
-	err = ioutil.WriteFile(filePath, data, 0777) //пишем данные на диск, 1 - путь до файла, 2 - слайс байт, 3 - доступ к заданному файлу
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Unable to save file", http.StatusInternalServerError)
-		return
-	}
-	fmt.Fprintf(w, "File %s has been successfully uploaded", header.Filename) //выводим сообщение об успешно загруженном файле
-}
-
-//add
